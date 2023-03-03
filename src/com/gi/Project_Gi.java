837ffd03fd16d338edce05fbfa27d1c46fef5804
Sandhyashree123 <msandya.sugu@gmail.com>
2023-03-03 20:44:27 +0530

    Hi

---
+++ src/com/gi/Project_Gi.java
@@ -0,0 +1,13 @@
+package com.gi;
+
+public class Project_Gi {
+
+	public static void main(String[] args) {
+		String s ="SAiRam";
+		System.out.println(s);
+		boolean contains = s.contains(s);
+		System.out.println(contains);
+
+	}
+
+}
